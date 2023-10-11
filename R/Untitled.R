7f9c50c08fabcbdd13bb3e9a68af12f782a8ec49
Reihaneh Farzinnia <reihaneh.farzinnia@manchester.ac.uk>
2023-10-11 16:41:52 +0100

    11/10 - test 2

--- R/Untitled.R
+++ R/Untitled.R
@@ -26,3 +26,20 @@ ggplot(gapminder_euro2007, aes(x = pop_e6, y = lifeExp)) +
 #   per capita?
 #2.Does the pattern look the same for countries in other continents, e.g. Asia?
 #3.Does the pattern look the same for years in the mid-late-20th century? 
+
+# Question 1 --------------------------------------------------------------
+#
+ggplot(gapminder, aes(x=gdpPercap, y=lifeExp)) + geom_point(size=2, shape=23)
+
+#Add the regression line
+ggplot(gapminder, aes(x = gdpPercap, y = lifeExp)) + geom_point() + 
+  geom_smooth(method=lm)
+
+#Remove the confidence interval
+ggplot(gapminder, aes(x=gdpPercap, y=lifeExp)) + geom_point() + 
+  geom_smooth(method=lm, se=FALSE)
+
+#Loess method - Fitting quadratic model 
+
+# Question 2 --------------------------------------------------------------
+
