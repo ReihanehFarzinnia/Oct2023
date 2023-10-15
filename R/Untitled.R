6ffa0aa185f2c6d98828d3e8ff65aa776e44cc0f
Reihaneh Farzinnia <reihaneh.farzinnia@manchester.ac.uk>
2023-10-15 10:44:02 +0100

    Outliers

--- R/Untitled.R
+++ R/Untitled.R
@@ -47,10 +47,28 @@ ggplot(gapminder_asia2007, aes(x = gdpPercap, y = lifeExp)) +
   geom_point(col = "red") +
   geom_smooth(method = lm, formula = y ~ x)
 
+#Find the outlier 
+#In R, you can identify outliers in a linear model by examining the residuals, 
+#which represent the differences between the observed values and the predicted 
+#values from your linear model. Outliers are typically data points associated 
+#with large or extreme residuals. 
+#Extract residuals from your model
+model <- lm(formula = gdpPercap ~ lifeExp, data = gapminder_asia2007)
+residuals <- resid(model)
+
+#Standardized Residuals:Calculate standardized residuals and consider values 
+#that are much larger or smaller than the typical range (e.g., beyond ±2 or ±3 
+#standard deviations) as potential outliers.
+std_residuals <- rstandard(model)
+outliers <- abs(std_residuals) > 2  # You can adjust the threshold as needed
+
+# Question 3 --------------------------------------------------------------
 
 
 
-ggplot(gapminder, aes(x=gdpPercap, y=lifeExp)) + geom_point(size=2, shape=23)
+
+
+
 
 #Add the regression line
 ggplot(gapminder, aes(x = gdpPercap, y = lifeExp)) + geom_point() + 
@@ -61,6 +79,3 @@ ggplot(gapminder, aes(x=gdpPercap, y=lifeExp)) + geom_point() +
   geom_smooth(method=lm, se=FALSE)
 
 #Loess method - Fitting quadratic model 
-
-# Question 2 --------------------------------------------------------------
-#Asia 
